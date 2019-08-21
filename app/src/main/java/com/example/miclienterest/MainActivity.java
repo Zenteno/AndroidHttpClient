124bf303525feae81871056d0e56fd548e33e33d
Zenteno <x.zenteno.a@gmail.com>
2019-08-21 12:34:44 -0400

    initial commit

---
+++ app/src/main/java/com/example/miclienterest/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.miclienterest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.JsonRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        RequestQueue queue = MySingleton.getInstance(this).getRequestQueue();
+        String url = "https://api.adderou.cl/tyaas/";
+
+        JsonObjectRequest jr = new JsonObjectRequest(Request.Method.GET, url,
+                null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                System.out.println(response.toString());
+            }
+        },null);
+        queue.add(jr);
+
+    }
+}
